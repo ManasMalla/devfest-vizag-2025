3e006becb95953d111bb94ea345dba1a124c20e6
Sampath Balivada <balivadask2000@gmail.com>
2025-06-27 16:42:13 +0000

    I don't see the background in the pages.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('min-h-screen bg-background font-body antialiased flex flex-col')}>
+      <body className={cn('min-h-screen font-body antialiased flex flex-col')}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
